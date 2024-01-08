bb614cd0e8545115c25d69a1677322cdf6635e1f
Alex Curtis <alexcrts298@gmail.com>
2024-01-08 15:37:07 +0000

    Fixed bugs and wording

--- shopping_cart.py
+++ shopping_cart.py
@@ -7,11 +7,11 @@ product_dict        =   {"1"   :   "Squeaky Toy",
                          "5"   :   "Cat Litter",
                         }
 
-price_dict           =  {"1"   :   2,
-                         "2"   :   4,
-                         "3"   :   4,
-                         "4"   :   3,
-                         "5"   :   2,
+price_dict           =  {"1"   :   2.99,
+                         "2"   :   4.99,
+                         "3"   :   4.49,
+                         "4"   :   3.79,
+                         "5"   :   2.99,
                         }
 
 cart_quantity_dict   =  {"1"   :   0,
@@ -21,12 +21,11 @@ cart_quantity_dict   =  {"1"   :   0,
                          "5"   :   0,
                         }
 
-display_line = "#" + "="*90 + "#"
 counter = 0
+display_line = "#" + "="*90 + "#"
 
 close_cart = False
 while not close_cart:
-    print(counter)
     print(display_line)
     print("Welcome to Pets, Pets, Pets, and also more Pets Ltd.!")
     print(display_line)
@@ -36,14 +35,14 @@ while not close_cart:
     for item in product_dict:
         if cart_quantity_dict[item] > 0:
             print(product_dict[item] + "\tx " + str(cart_quantity_dict[item]) + "\t£" + 
-                  str(price_dict[item] * cart_quantity_dict[item]), sep='',end='\n')
+                  str(round(price_dict[item], 2) * cart_quantity_dict[item]), sep='',end='\n')
     print(display_line)
 
     # Cart price display
     total_cart_cost = 0     # Reset total for fresh calculation
     for x in price_dict:
         total_cart_cost += price_dict.get(x) * cart_quantity_dict.get(x)
-    print(f"Your current total is:\t£{total_cart_cost}\n" + display_line)
+    print(f"Your current total is:\t£{round(total_cart_cost, 2)}\n" + display_line)
 
 
     print("1. Add an item to your basket")
@@ -69,12 +68,16 @@ while not close_cart:
                 add_item_validation = True
                 os.system('cls')
             except IndexError:
-                print("Invalid input, please try again.")
-            except ValueError:
-                print("Invalid input, please try again.")
+                print("ERROR: Invalid input, please try again.")
+            except KeyError:
+                print("ERROR: Invalid input, please try again.")
 
+    # Option 2 - Remove item
     elif menu_select == "2":
-        # print(display_line)
+        if counter < 1:
+            print("ERROR: No items in basket to remove")
+            continue
+
         for i, item in enumerate(product_dict, 1):
             if cart_quantity_dict[item] > 0:
                 print(i, '. ' + product_dict[item] + "\t\t x " + str(cart_quantity_dict[item]), sep='',end='\n'
@@ -83,35 +86,41 @@ while not close_cart:
         remove_item_validation = False
         while not remove_item_validation:
             try:
-                remove_item = str(input("Please enter the corresponding number to remove an item (-1 to go back): "))
-                if remove_item == -1:
-                    break
-                elif remove_item > counter:
+                remove_item = input("Please enter the corresponding number to remove an item: ")
+                if cart_quantity_dict[remove_item] < 1:
                     raise Exception
+                if remove_item.isnumeric == False:
+                    raise Exception
+
                 cart_quantity_dict[remove_item] -= 1
                 counter -= 1
-                remove_item_validation = True
-                os.system('cls')
+            
             except Exception:
-                print("Invalid input, please try again.")            
-    
+                print("ERROR: Invalid input, please try again.")   
+
+            finally:
+                remove_item_validation = True 
+                os.system('cls')   
+
+    # Option 3 - Checkout
     elif menu_select == "3":
         print(display_line + 
-              f"\nYour total to pay is:\t £{round(total_cart_cost)}\n"
+              f"\nYour total to pay is:\t £{round(total_cart_cost, 2)}\n"
               f"{display_line}\n"
               f"1. Checkout and close.\n"
               f"2. Main menu\n"
               f"{display_line}")
+        
         main_menu = int(input("Please select either 1 or 2: "))
         if main_menu == 1:
             close_cart = True
+            os.system('cls')
             print(display_line +
                 "\nThank you for visiting Pets, Pets, Pets, and also more Pets Ltd.!"
-                "\nPlease come again soon!\n" +
-                display_line)
+                "\nPlease come again soon!\n" + display_line)
         else:
-            pass
+            os.system('cls')
     else:
         os.system('cls')
-        print("Invalid input, please try again.")
+        print("ERROR: Invalid input, please try again.")
 
